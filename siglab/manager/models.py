203aecf223299709467e2e52a577f135fb6a4308
Matheus0820 <mr7052954@gmail.com>
2024-09-30 17:03:45 -0300

    Códigos iniciais

--- siglab/manager/models.py
+++ siglab/manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Bolsista(models.Model):
+    nome = 0
+    matricula = 0
+    curso = 0
+    grauEscolar = 0
+    tipoDeBolsa = 0
+
+class Horario(models.Model):
+    diaSemana = 0
+    horarioDia = 0
+
+class Presenca(models.Model):
+    bolsista = 0
+    data = 0
+    horaEntrada = 0
+    horaSaida = 0
